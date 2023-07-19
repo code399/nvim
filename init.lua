ce3ea4f1d2ce2517e0473170f0b3ca453f674e12
code399 <code399@163.com>
2023-07-19 22:29:11 +0800

    Great nvim config files

---
+++ init.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- 复制后高亮提示
+-- See `:help vim.highlight.on_yank()`
+local h = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd(
+  'TextYankPost',
+  {
+    callback = function()
+      vim.highlight.on_yank({timeout = 300})
+    end,
+    group = h,
+    pattern = '*',
+  }
+)
+
+require("options")
+-- require("keymaps")
+require("plugins")
---
+++ lua/plugins.lua
@@ -0,0 +1,41 @@
+local plugins = {
+  -- https://github.com/navarasu/onedark.nvim
+  -- 配色方案
+  { "navarasu/onedark.nvim", lazy = false, priority = 1000 },
+
+  -- https://github.com/max397574/better-escape.nvim
+  -- ESC 键替换方案
+  { "max397574/better-escape.nvim" },
+}
+
+--------------------------------------------------------------------------- Lazy
+-- https://github.com/folke/lazy.nvim
+-- 插件管理器 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup (plugins)
+
+-------------------------------------------------------------------- Colorscheme
+require('onedark').setup { style = 'dark' }
+-- dark,darker,cool,deep,warm,warmer,light
+
+vim.cmd('colorscheme onedark')
+
+------------------------------------------------------------------ Better escape
+require("better_escape").setup {
+  mapping = {"kk", "jj"},
+  timeout = vim.o.timeoutlen,
+  clear_empty_lines = false,
+  keys = "<Esc>"
+}
