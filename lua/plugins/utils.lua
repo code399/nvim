22f693962949960759d8dc9932a02f04fcbb8a5f
code399 <code399@163.com>
2023-07-23 21:08:26 +0800

    Add plugin 'folke/flash.nvim'

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -14,5 +14,29 @@ return {
             require 'nvim-lastplace'.setup() 
         end
 
+    },
+
+    {   -- https://github.com/folke/flash.nvim -- 光标跳转
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        config = function()
+            local map = vim.keymap.set
+            local flash = require("flash")  
+            flash.setup()
+            -- map({"n", "x", "o"}, "s", function()
+            --     flash.jump({
+            --         search = {
+            --             mode = function(str)
+            --                 return "\\<" .. str
+            --             end
+            --         },
+            --     })
+            -- end, {}
+            map( { "n", "x", "o" }, "s", function() flash.jump() end, {desc = "Flash"} )
+            map( { "n", "o", "x" }, "S", function() flash.treesitter() end, {desc = "Flash Treesitter"} )
+            map( "o", "r", function() flash.remote() end, {desc = "Remote Flash"} )
+            map( { "o", "x" }, "R", function() flash.treesitter_search() end, {desc = "Treesitter Search"} )
+            map( { "c" }, "<c-s>", function() flash.toggle() end, {desc = "Toggle Flash Search"} )
+        end
     }
 }
